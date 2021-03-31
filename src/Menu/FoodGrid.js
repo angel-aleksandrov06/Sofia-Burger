2b0c64897063bc03ce9e4e8c71c1750170a0b672
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-03-31 16:25:46 +0300

    Add css transitions on Food hover

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -3,7 +3,7 @@ import { Title } from '../Styles/title';
 
 export const FoodGrid = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 20px;
 `;
 
@@ -22,9 +22,15 @@ export const Food = styled(Title)`
     background-size: cover;
     filter: contrast(65%);
     border-radius: 7px;
-    box-shadow: 0px 0px 10px 0px grey;
+    margin-top: 5px;
+    transition-property: box-shadow margin-top filter;
+    transition-duration: 0.2s;
+    box-shadow: 0px 0px 3px 0px grey;
     &:hover {
         cursor: pointer;
-        opacity: 0.7;
+        filter: contrast(100%);
+        margin-top: 0px;
+        margin-bottom: 5px;
+        box-shadow: 0px 0px 10px 0px grey;
     }
 `;
